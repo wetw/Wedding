9440068c6fdcc43a27c2996e84346fdbcbdf5e98
weiting <a26007565@gmail.com>
2020-12-20 22:07:43 +0800

    init

---
+++ Controllers/LineController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLineBotSDK;
+using NetCoreLineBotSDK.Filters;
+using NetCoreLineBotSDK.Models;
+using System.Threading.Tasks;
+
+namespace Wedding.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineController : ControllerBase
+    {
+        private readonly LineBotApp _app;
+
+        public LineController(LineBotApp app)
+        {
+            _app = app;
+        }
+
+        [HttpPost]
+        [LineVerifySignature]
+        public async Task<IActionResult> Post(WebhookEvent request)
+        {
+            await _app.RunAsync(request.events).ConfigureAwait(false);
+            return Ok();
+        }
+    }
+}
---
+++ Services/LineBot/WeddingLineBotApp.cs
@@ -0,0 +1,32 @@
+﻿using NetCoreLineBotSDK;
+using NetCoreLineBotSDK.Enums;
+using NetCoreLineBotSDK.Interfaces;
+using NetCoreLineBotSDK.Models.LineObject;
+using System.Threading.Tasks;
+
+namespace Wedding.Services.LineBot
+{
+    public class WeddingLineBotApp : LineBotApp
+    {
+        private readonly ILineMessageUtility _lineMessageUtility;
+
+        public WeddingLineBotApp(ILineMessageUtility lineMessageUtility) : base(lineMessageUtility)
+        {
+            _lineMessageUtility = lineMessageUtility;
+        }
+
+        protected override async Task OnMessageAsync(LineEvent ev)
+        {
+            await _lineMessageUtility.ReplyMessageAsync(ev.replyToken, $"You Said:{ev.message.Text}");
+        }
+
+        protected override async Task OnBeaconAsync(LineEvent ev)
+        {
+            if (ev.beacon.type == BeaconType.enter)
+            {
+                var userProfile= await _lineMessageUtility.GetUserProfile(ev.source.userId).ConfigureAwait(false);
+                await _lineMessageUtility.ReplyMessageAsync(ev.replyToken, $"歡迎蒞臨\r\n{userProfile.displayName}").ConfigureAwait(false);
+            }
+        }
+    }
+}
