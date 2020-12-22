42c6691dcb952184440f4cd41e676da2bddafc5f
weiting <a26007565@gmail.com>
2020-12-22 09:59:04 +0800

    加上樣板但還沒修正完成

---
+++ Data/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace Wedding.Data
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Number { get; set; }
+
+        public RelationType Relation { get; set; }
+    }
+
+    public enum RelationType
+    {
+        Man,
+        Woman
+    }
+}
