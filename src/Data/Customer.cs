53a645f41f78ffdf660be00e254ee1b9967d17e7
weiting <a26007565@gmail.com>
2021-06-22 01:22:13 +0800

    Add more customer info

--- src/Data/Customer.cs
+++ src/Data/Customer.cs
@@ -1,10 +1,10 @@
-using System.ComponentModel.DataAnnotations;
 using SqlSugar;
+using System.ComponentModel.DataAnnotations;
 
 namespace Wedding.Data
 {
     [SugarTable(nameof(Customer))]
-    public class Customer
+    public class Customer : BaseEntity
     {
         [SugarColumn(IsPrimaryKey = true, IsIdentity = true, IndexGroupNameList = new[] { "customer_id_index1" })]
         public int Id { get; set; }
@@ -25,10 +25,28 @@ namespace Wedding.Data
         [SugarColumn(IsNullable = true)]
         public string Phone { get; set; }
 
+        public bool IsAttend { get; set; } = true;
+
+        [Range(0, 10)]
         [SugarColumn(IsNullable = true)]
-        public int Visitors { get; set; }
+        public int Visitors { get; set; } = 1;
+
+        [Range(0, 10)]
+        public int ChildrenCount { get; set; } = 0;
 
         public RelationType Relation { get; set; } = RelationType.Man;
+
+        public bool IsRealBook { get; set; } = false;
+
+        [SugarColumn(IsNullable = true)]
+        public string Address { get; set; }
+
+        public bool IsVegetarian { get; set; } = false;
+
+        [Range(0, 10)]
+        public int VegetarianCount { get; set; } = 0;
+
+        public string Table { get; set; }
     }
 
     public enum RelationType
