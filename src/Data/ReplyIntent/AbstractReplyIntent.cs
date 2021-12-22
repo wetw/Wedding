9c46ad4f161c58f7087fda83fe502fd7a2520bb3
weiting <a26007565@gmail.com>
2021-12-23 01:05:37 +0800

    優化樣板訊息拿取方式，並支援回傳多筆訊息樣板
    - 移除SDK: NetCoreLineBotSDK，改使用 submodules 方式
    - FlexMessage 支援 QuickReply

--- src/Data/ReplyIntent/AbstractReplyIntent.cs
+++ src/Data/ReplyIntent/AbstractReplyIntent.cs
@@ -27,65 +27,76 @@ namespace Wedding.Data.ReplyIntent
 
         public abstract Task ReplyAsync(LineEvent ev);
 
+        /// <summary>
+        /// 當未傳入 filterName 時，抓取資料夾下的所有訊息樣板
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="filterName"></param>
+        /// <returns></returns>
         protected async Task<List<IRequestMessage>> GetTemplateMessages(string path, string filterName = null)
         {
-            using var intents = _fileProvider.GetDirectoryContents(path).GetEnumerator();
+            using var intents = _fileProvider.GetDirectoryContents(path)
+                .Where(x => string.IsNullOrWhiteSpace(filterName) || filterName.Equals(x.Name))
+                .GetEnumerator();
             var messages = new List<IRequestMessage>();
-            while (intents.MoveNext())
+
+            while (intents.MoveNext() && intents.Current is { IsDirectory: false })
             {
-                if (intents.Current is { IsDirectory: false })
+                var json = await File.ReadAllTextAsync(intents.Current.PhysicalPath).ConfigureAwait(false);
+                switch (_templateNamePattern.Match(intents.Current.Name).Groups["templateName"].Value)
                 {
-                    if (!string.IsNullOrWhiteSpace(filterName)
-                        && !filterName.Equals(intents.Current.Name))
-                    {
-                        continue;
-                    }
-                    var json = await File.ReadAllTextAsync(intents.Current.PhysicalPath).ConfigureAwait(false);
-                    switch (_templateNamePattern.Match(intents.Current.Name).Groups["templateName"].Value)
-                    {
-                        case nameof(AudioMessage):
-                            messages.Add(JsonConvert.DeserializeObject<AudioMessage>(json));
-                            break;
-                        case nameof(FlexMessage):
-                            var template = JsonDocument.Parse(json).RootElement.EnumerateObject();
-                            var altText= template.FirstOrDefault(c => c.Name.Equals("altText", StringComparison.OrdinalIgnoreCase)).Value.ToString();
-                            var contents = template.FirstOrDefault(c => c.Name.Equals("contents", StringComparison.OrdinalIgnoreCase)).Value.ToString();
-                            messages.Add(new FlexMessage(contents, altText));
-                            break;
-                        case nameof(ImageMessage):
-                            messages.Add(JsonConvert.DeserializeObject<ImageMessage>(json));
-                            break;
-                        case nameof(LocationMessage):
-                            messages.Add(JsonConvert.DeserializeObject<LocationMessage>(json));
-                            break;
-                        case nameof(StickerMessage):
-                            messages.Add(JsonConvert.DeserializeObject<StickerMessage>(json));
-                            break;
-                        case nameof(TextMessage):
-                            messages.Add(JsonConvert.DeserializeObject<TextMessage>(json));
-                            break;
-                        case nameof(VideoMessage):
-                            messages.Add(JsonConvert.DeserializeObject<VideoMessage>(json));
-                            break;
-                        case nameof(ButtonTemplate):
-                            messages.Add(JsonConvert.DeserializeObject<ButtonTemplate>(json));
-                            break;
-                        case nameof(CarouselTemplate):
-                            messages.Add(JsonConvert.DeserializeObject<CarouselTemplate>(json));
-                            break;
-                        case nameof(ConfirmTemplate):
-                            messages.Add(JsonConvert.DeserializeObject<ConfirmTemplate>(json));
-                            break;
-                        case nameof(ImageCarouselTemplate):
-                            messages.Add(JsonConvert.DeserializeObject<ImageCarouselTemplate>(json));
-                            break;
-                    }
+                    case nameof(AudioMessage):
+                        messages.Add(JsonConvert.DeserializeObject<AudioMessage>(json));
+                        break;
+                    case nameof(FlexMessage):
+                        var template = JsonDocument.Parse(json).RootElement.EnumerateObject();
+                        var altText = template.FirstOrDefault(c => c.Name.Equals("altText", StringComparison.OrdinalIgnoreCase)).Value.ToString();
+                        var contents = template.FirstOrDefault(c => c.Name.Equals("contents", StringComparison.OrdinalIgnoreCase)).Value.ToString();
+                        var quickReplyString = template.FirstOrDefault(c => c.Name.Equals("QuickReply", StringComparison.OrdinalIgnoreCase)).Value.ToString();
+                        var quickReplay = quickReplyString?.Length > 0 ? JsonConvert.DeserializeObject<QuickReply>(quickReplyString) : null;
+                        messages.Add(new FlexMessage(contents, altText, quickReplay));
+                        break;
+                    case nameof(ImageMessage):
+                        messages.Add(JsonConvert.DeserializeObject<ImageMessage>(json));
+                        break;
+                    case nameof(LocationMessage):
+                        messages.Add(JsonConvert.DeserializeObject<LocationMessage>(json));
+                        break;
+                    case nameof(StickerMessage):
+                        messages.Add(JsonConvert.DeserializeObject<StickerMessage>(json));
+                        break;
+                    case nameof(TextMessage):
+                        messages.Add(JsonConvert.DeserializeObject<TextMessage>(json));
+                        break;
+                    case nameof(VideoMessage):
+                        messages.Add(JsonConvert.DeserializeObject<VideoMessage>(json));
+                        break;
+                    case nameof(ButtonTemplate):
+                        messages.Add(JsonConvert.DeserializeObject<ButtonTemplate>(json));
+                        break;
+                    case nameof(CarouselTemplate):
+                        messages.Add(JsonConvert.DeserializeObject<CarouselTemplate>(json));
+                        break;
+                    case nameof(ConfirmTemplate):
+                        messages.Add(JsonConvert.DeserializeObject<ConfirmTemplate>(json));
+                        break;
+                    case nameof(ImageCarouselTemplate):
+                        messages.Add(JsonConvert.DeserializeObject<ImageCarouselTemplate>(json));
+                        break;
                 }
             }
 
             return messages;
         }
 
+        /// <summary>
+        /// 嘗試拿訊息樣板，沒拿到時會回傳文字
+        /// </summary>
+        /// <param name="ev"></param>
+        /// <param name="value">可傳入多筆樣板，藉由 ; 來分隔</param>
+        /// <param name="templateFolderPath"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         protected async Task TryGetTemplateMessageAsync(LineEvent ev, string value, string templateFolderPath)
         {
             if (ev == null)
@@ -103,15 +114,25 @@ namespace Wedding.Data.ReplyIntent
                 throw new ArgumentNullException(nameof(templateFolderPath));
             }
 
-            // Intent_*_StickerMessage.json mean try get template from folder(templateFolderPath).
-            if (value.StartsWith("Intent_") && value.EndsWith(".json"))
+
+            var messages = new List<IRequestMessage>();
+            foreach (var item in value.Split(';'))
             {
-                var messages = await GetTemplateMessages(templateFolderPath, value).ConfigureAwait(false);
-                // if template file not found, will return text message.
-                await LineMessageUtility.ReplyMessageAsync(ev.replyToken,
-                    messages.Count > 0 ? messages
-                        : new List<IRequestMessage> { new TextMessage(value) }).ConfigureAwait(false);
+                if (item.StartsWith("Intent_") && item.EndsWith(".json"))
+                {
+                    // Intent_*_StickerMessage.json mean try get template from folder(templateFolderPath).
+                    var template = await GetTemplateMessages(templateFolderPath, item).ConfigureAwait(false);
+                    if (template?.Any() == true)
+                    {
+                        messages.AddRange(template);
+                    }
+                }
             }
+
+            // if template file not found, will return text message.
+            await LineMessageUtility.ReplyMessageAsync(ev.replyToken,
+                messages.Count > 0 ? messages
+                    : new List<IRequestMessage> { new TextMessage(value) }).ConfigureAwait(false);
             await LineMessageUtility.ReplyMessageAsync(ev.replyToken, value).ConfigureAwait(false);
         }
     }
