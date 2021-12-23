dce92a0d6006b848f03fa895b5185a4edf5bbe6a
wilber_chen <wilber_chen@gss.com.tw>
2021-12-23 21:55:37 +0800

    新增婚紗照輪撥

--- src/Data/WeddingOptions.cs
+++ src/Data/WeddingOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Wedding.Data
 {
@@ -16,5 +17,6 @@ namespace Wedding.Data
 
         public string InvitationDescription { get; init; }
 
+        public IReadOnlyCollection<string> WeddingPhotos { get; init; }
     }
 }
