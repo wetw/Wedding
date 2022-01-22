b7d7fbe6f573339a2f482efe908f375af8bfef9d
weiting <a26007565@gmail.com>
2022-01-22 21:56:23 +0800

    feat: Add Grid.Blazor
    https://github.com/gustavnavar/Grid.Blazor

--- src/Pages/ManagePage.razor.cs
+++ src/Pages/ManagePage.razor.cs
@@ -1,7 +1,10 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using GridBlazor;
+using GridShared;
+using GridShared.Utility;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Primitives;
 using Wedding.Data;
 using Wedding.Services;
 
@@ -9,7 +12,6 @@ namespace Wedding.Pages
 {
     partial class ManagePage
     {
-
         [Inject]
         private ICustomerDao CustomerDao { get; init; }
 
@@ -19,7 +21,25 @@ namespace Wedding.Pages
         [CascadingParameter]
         private Task<AuthenticationState> AuthenticationStateTask { get; init; }
 
-        private IList<Customer> _customers;
+        private CGrid<Customer> _grid;
+
+        private static void Columns(IGridColumnCollection<Customer> c)
+        {
+            c.Add(x => x.Id);
+            c.Add(x => x.IsAttend).Titled("參加");
+            c.Add(x => x.Name).Titled("Line 名稱");
+            c.Add(x => x.RealName).Titled("名稱");
+            c.Add(x => x.Relation).Titled("關係");
+            c.Add(x => x.IsEmailBook).Titled("電子喜帖").Sum(true);
+            c.Add(x => x.Email).Titled("信箱");
+            c.Add(x => x.IsRealBook).Titled("實體喜帖").Sum(true);
+            c.Add(x => x.PostCode).Titled("郵遞區號");
+            c.Add(x => x.Address).Titled("地址");
+            c.Add(x => x.Visitors).Titled("大人人數").Sum(true);
+            c.Add(x => x.ChildrenCount).Titled("小孩人數").Sum(true);
+            c.Add(x => x.VegetarianCount).Titled("吃素人數").Sum(true);
+            c.Add(x => x.LastModifyTime).Titled("最後更新時間").SetFilterWidgetType("DateTimeLocal");
+        }
 
         protected override async Task OnInitializedAsync()
         {
@@ -34,7 +54,28 @@ namespace Wedding.Pages
                 }
                 NavigationManager.NavigateTo($"/api/line/login?returnUrl={returnUrl}", true);
             }
-            _customers = await CustomerDao.GetListAsync(1, 10).ConfigureAwait(false);
+        }
+
+        protected override Task OnParametersSetAsync()
+        {
+            var query = new QueryDictionary<StringValues>();
+            var client = new GridClient<Customer>(q => CustomerDao.GetGridRowsAsync(Columns, q), query, false, "customersGrid", Columns)
+                .SetRowCssClasses(customer => customer.IsAttend == null ? "IsAttendNull" : string.Empty)
+                .ChangePageSize(true)
+                .Sortable()
+                .Filterable()
+                .WithMultipleFilters()
+                .Groupable(true)
+                .ClearFiltersButton(true)
+                .SetStriped(true)
+                .WithGridItemsCount()
+                .SetExcelExport(true, false, "Customers")
+                .Searchable(true, false, true);
+            _grid = client.Grid;
+
+            // Set new items to grid
+            _ = client.UpdateGrid();
+            return Task.CompletedTask;
         }
     }
 
